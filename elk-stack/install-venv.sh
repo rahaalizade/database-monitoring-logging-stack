08f31268c7435c0079eb4213fe6f61e4a2eab982
raha.alizadeh <fatemeh.alizadeh@digikal.com>
2025-08-15 19:42:00 +0330

    remove unnecassary files

--- elk-stack/install-venv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DEBIAN_FRONTEND="noninteractive" sudo apt -yq install python3-pip
-sudo pip3 install virtualenv
-VENV_DIR=$(pwd)/.env
-virtualenv "$VENV_DIR"
-source .env/bin/activate
-pip3 install -r requirements.txt
-ansible-galaxy collection install ansible.posix community.docker
-echo "After running this script, Please execute 'source ./.env/bin/activate'"
